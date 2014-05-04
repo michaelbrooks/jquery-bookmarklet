fc5100c2f6d8e7c7e2083ab2149ff32baef5cfcb
Michael Brooks <mjbrooks@uw.edu>
2014-05-04 14:13:57 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-jquery-bookmarklet
-==================
+# jQuery Bookmarklet Plugin [![Build Status](https://secure.travis-ci.org/michaelbroks/jquery-bookmarklet.svg?branch=master)](https://travis-ci.org/michaelbrooks/jquery-bookmarklet)
 
-jQuery plugin to create a bookmarklet from JS file or Gist.
+### Make Bookmarklets from JavaScript files
+
+According to Wikipedia, a [bookmarklet](http://en.wikipedia.org/wiki/Bookmarklet)
+is a bookmark stored in a web browser that contains JavaScript commands to extend the browser's functionality.
+
+Usually bookmarklets are created when the user drags and drops a
+special anchor (`<a>`) element into their bookmarks toolbar.
+The anchor's `href` attribute is set to `javascript:<the code to execute>`.
+
+This makes bookmarklets kind of irritating to maintain, since you have
+to prepare URL-encoded JavaScript and add it to your
+`<a>` element every time you update the script.
+
+This jQuery plugin loads your bookmarklet's JavaScript from an external file,
+and then augments an anchor element on your page
+with the appropriate url.
+It can also load JavaScript from a Gist via
+the GitHub API.
+
+## Usage
+
+1. Include jQuery:
+
+	```html
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	```
+
+2. Include plugin's code:
+
+	```html
+	<script src="dist/jquery.bookmarklet.min.js"></script>
+	```
+
+3. Add an anchor to your HTML:
+
+    ```html
+    <a id="element">My Bookmarklet</a>
+    ```
+
+4. Call the plugin:
+
+	```javascript
+	$("#element").bookmarklet({
+		raw: "path/to/your/script.js"
+	});
+	```
+
+    Alternatively, point the plugin at a Gist:
+
+    ```javascript
+    $("#element").bookmarklet({
+        gist_id: "MY_GIST_ID",
+        gist_filename: "filename.js"
+        /* gist_filename is optional if the Gist contains one file */
+    });
+    ```
+
+    You can get your Gist's id from its url: `https://gist.github.com/<username>/<gist_id>`.
---
+++ src/jquery.bookmarklet.js
@@ -0,0 +1,92 @@
+//Thanks to the jQuery Plugin Boilerplate project (http://jqueryboilerplate.com/)
+;(function ( $, window, document, undefined ) {
+
+    // Create the defaults once
+    var pluginName = "bookmarklet",
+        defaults = {
+            //Probably do not need to change this
+            gist_api_url: "https://api.github.com/gists/"
+        };
+
+    var handlers = {
+        "gist": function() {
+            var gist_id = this.settings.gist_id;
+            var api_url = this.settings.gist_api_url;
+            var filename = this.settings.gist_filename;
+
+            return $.getJSON(api_url + gist_id)
+                .then(function(gist) {
+                    var file;
+                    if (filename) {
+                        file = gist.files[filename];
+                    } else if (Object.keys(gist.files).length === 1) {
+                        file = gist.files[Object.keys(gist.files)[0]];
+                    } else {
+                        throw "Gist contains multiple files. Use 'gist_filename' setting to specify.";
+                    }
+
+                    return file.content;
+                });
+        },
+        "raw": function() {
+            return $.get(this.settings.raw);
+        }
+    };
+
+    // The actual plugin constructor
+    function Bookmarklet ( element, options ) {
+        this.element = element;
+        this.$element = $(this.element);
+
+        //Get some settings from the element's data attributes
+        var attrs = $.extend({}, {
+            gist_id: this.$element.data("bookmarklet-gist-id"),
+            gist_filename: this.$element.data("bookmarklet-gist-filename"),
+            raw: this.$element.data("bookmarklet-raw")
+        });
+
+        this.settings = $.extend({}, defaults, attrs, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Bookmarklet.prototype = {
+        init: function () {
+            var self = this;
+            var request;
+            if (this.settings.raw) {
+                request = handlers.raw.call(this);
+            } else if (this.settings.gist_id) {
+                request = handlers.gist.call(this);
+            } else {
+                throw "Bookmarklet settings must include either 'gist_id' or 'raw'.";
+            }
+
+            request
+                .done(function(code) {
+                    self.$element.attr("href", self.build_url(code));
+                })
+                .fail(function(error) {
+                    throw error;
+                });
+        },
+        build_url: function(code) {
+            return "javascript:" + encodeURIComponent(code);
+        }
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[ pluginName ] = function ( options ) {
+        this.each(function() {
+            if ( !$.data( this, "plugin_" + pluginName ) ) {
+                $.data( this, "plugin_" + pluginName, new Bookmarklet( this, options ) );
+            }
+        });
+
+        // chain jQuery functions
+        return this;
+    };
+
+})( jQuery, window, document );
